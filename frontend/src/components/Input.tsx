1f89901be3998379ef92d445466e27029a9bc05f
itelo filho <itelofilho@gmail.com>
2019-07-12 11:45:13 -0300

    add basic struct of component, need refactor

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import InputBase from "@material-ui/core/InputBase";
 import Divider from "@material-ui/core/Divider";
@@ -8,13 +8,32 @@ import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
 import DirectionsIcon from "@material-ui/icons/Directions";
 
-const useStyles = makeStyles(
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import { Button } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    fixButtonPosition: {
+      marginTop: -8
+    },
+    space: {
+      width: 4,
+      height: "100%"
+    },
     root: {
       padding: "2px 4px",
       display: "flex",
-      alignItems: "center",
+      flexDirection: "column",
+      // alignItems: "center",
       width: 400
+    },
+    row: {
+      display: "flex",
+      flexDirection: "row"
     },
     input: {
       marginLeft: 8,
@@ -27,35 +46,123 @@ const useStyles = makeStyles(
       width: 1,
       height: 28,
       margin: 4
+    },
+    root2: {
+      display: "flex"
+    },
+    paper: {
+      marginRight: theme.spacing(2)
     }
   })
 );
 
+const messages = {
+  2: "2 min",
+  10: "10 min",
+  30: "30 min"
+} as {
+  2: string;
+  10: string;
+  30: string;
+};
+
+type IntervalValues = 2 | 10 | 30;
+
 export default function CustomizedInputBase() {
-  // @ts-ignore
   const classes = useStyles();
 
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef<HTMLButtonElement>(null);
+  const [interval, setInterval] = React.useState(10 as IntervalValues);
+
+  function handleToggle() {
+    setOpen(prevOpen => !prevOpen);
+  }
+
+  const handleClose = (intervalDesired?: IntervalValues) => (
+    event: React.MouseEvent<EventTarget>
+  ) => {
+    if (intervalDesired) {
+      setInterval(intervalDesired);
+    }
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      // @ts-ignore
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
-    <Paper className={classes.root}>
-      <IconButton className={classes.iconButton} aria-label="Menu">
-        <MenuIcon />
-      </IconButton>
-      <InputBase
-        className={classes.input}
-        placeholder="Search Google Maps"
-        inputProps={{ "aria-label": "Search Google Maps" }}
-      />
-      <IconButton className={classes.iconButton} aria-label="Search">
-        <SearchIcon />
-      </IconButton>
-      <Divider className={classes.divider} />
-      <IconButton
-        color="primary"
-        className={classes.iconButton}
-        aria-label="Directions"
-      >
-        <DirectionsIcon />
-      </IconButton>
-    </Paper>
+    <div className={classes.row}>
+      <Paper className={classes.root}>
+        <InputBase
+          className={classes.input}
+          placeholder="Email"
+          inputProps={{ "aria-label": "email", type: "email" }}
+        />
+        <div className={classes.row}>
+          <InputBase
+            className={classes.input}
+            placeholder="Search Phrase"
+            inputProps={{ "aria-label": "Search Phrase" }}
+          />
+          <Divider className={classes.divider} />
+          <div className={classes.root2}>
+            <div>
+              <Button
+                className={classes.fixButtonPosition}
+                ref={anchorRef}
+                aria-controls="menu-list-grow"
+                aria-haspopup="true"
+                onClick={handleToggle}
+              >
+                {messages[interval]}
+              </Button>
+              <Popper
+                open={open}
+                anchorEl={anchorRef.current}
+                keepMounted={false}
+                transition
+                disablePortal
+              >
+                {({ TransitionProps, placement }) => (
+                  <Grow
+                    {...TransitionProps}
+                    style={{
+                      transformOrigin:
+                        placement === "bottom" ? "center top" : "center bottom"
+                    }}
+                  >
+                    <Paper id="menu-list-grow">
+                      <ClickAwayListener onClickAway={handleClose()}>
+                        <MenuList>
+                          <MenuItem onClick={handleClose(2)}>
+                            2 minutos
+                          </MenuItem>
+                          <MenuItem onClick={handleClose(10)}>
+                            10 minutos
+                          </MenuItem>
+                          <MenuItem onClick={handleClose(30)}>
+                            30 minutos
+                          </MenuItem>
+                        </MenuList>
+                      </ClickAwayListener>
+                    </Paper>
+                  </Grow>
+                )}
+              </Popper>
+            </div>
+          </div>
+        </div>
+      </Paper>
+      <div className={classes.space} />
+      <Button variant="contained" color="primary">
+        Registrar
+      </Button>
+    </div>
   );
 }
